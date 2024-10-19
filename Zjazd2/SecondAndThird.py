36563e23fff9cefa5f4d0a42f59bc9420daa6579
Sensor <mateusz.lyson@blumentmt.com>
2024-10-19 09:23:54 +0200

    Second Task

--- Zjazd2/SecondAndThird.py
+++ Zjazd2/SecondAndThird.py
@@ -1,7 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 19 08:38:05 2024
+import pandas as pd
 
-@author: Sensorbtf
-"""
+# Ścieżka do pliku CSV
+csv_file_path = r"C:\Users\Sensorbtf\OneDrive\studia\Magisterka\Dane\IHME_GBD_2019_SMOKING_AGE_1990_2019_INIT_AGE_Y2021M05D27.csv"
+df = pd.read_csv(csv_file_path)
+df_cleaned = df.dropna(subset=['year_id', 'val'])
 
+# Obliczamy średnie rozpowszechnienie palenia dla każdej płci
+mean_prevalence_by_sex = df_cleaned.groupby('sex_name')['val'].mean().round(2)
+print("\n===== Średnie rozpowszechnienie palenia w zależności od płci =====\n")
+print(mean_prevalence_by_sex)
+
+# 2. Obliczamy medianę rozpowszechnienia palenia (z użyciem funkcji round() z Pythona)
+median_prevalence = round(df_cleaned['val'].median(), 2)
+print("\n===== Mediana rozpowszechnienia palenia =====\n")
+print(f"Mediana rozpowszechnienia palenia: {median_prevalence}")
+
+# 3. Obliczamy odchylenie standardowe dla każdej płci
+std_prevalence_by_sex = df_cleaned.groupby('sex_name')['val'].std().round(2)
+print("\n===== Odchylenie standardowe rozpowszechnienia palenia w zależności od płci =====\n")
+print(std_prevalence_by_sex)
+
+# 4. Obliczamy kowariancję między 'sex_id' a 'val' (rozpowszechnieniem palenia)
+covariance_sex = df_cleaned[['sex_id', 'val']].cov().round(4)
+print("\n===== Kowariancja między płcią a rozpowszechnieniem palenia =====\n")
+print(covariance_sex)
+
+# 5. Obliczamy korelację między 'sex_id' a 'val' (rozpowszechnieniem palenia)
+correlation_sex = df_cleaned['sex_id'].corr(df_cleaned['val']).round(4)
+print("\n===== Korelacja między płcią a rozpowszechnieniem palenia =====\n")
+print(f"Korelacja między płcią a rozpowszechnieniem palenia: {correlation_sex}")
